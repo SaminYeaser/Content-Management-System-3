52192c2013df3e51407258a73961d30fea37467f
Samin Yeaser <saminyeaser1@gmail.com>
2019-11-09 13:43:16 +0600

    Now we can delete in comment page??

--- admin/comment.php
+++ admin/comment.php
@@ -88,7 +88,7 @@
         echo "<td>$comment_date</td>";
         echo "<td><a href='comments.php?approve={$comment_id}'>Approve</a></td>";
         echo "<td><a href='comments.php?unapprove={$comment_id}'>Unapprove</a></td>";
-        echo "<td><a href='comments.php?delete={$comment_id}'>Delete</a></td>";
+        echo "<td><a href='comment.php?delete=$comment_id&id=".$_GET['id']."'>Delete</a></td>";
         echo "</tr>";
 
     }
@@ -132,7 +132,7 @@ if(isset($_GET['delete'])){
     $the_comment_id = $_GET['delete'];
     $query = "DELETE FROM comments WHERE comment_id ={$the_comment_id}";
     $delete_comment = mysqli_query($connection, $query);
-    header("Location: comments.php");
+    header("Location: comment.php?id=". $_GET['id'] ."");
 }
 
 ?>
