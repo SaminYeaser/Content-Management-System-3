2ade34b9ba8ec60ee1607126b01580997a8c8dbf
Samin Yeaser <saminyeaser1@gmail.com>
2019-11-15 01:01:01 +0600

    processing for the contact us page activation!!!

--- includes/navigation.php
+++ includes/navigation.php
@@ -32,7 +32,7 @@
                             $category_class = 'active';
                         }else if ($pagename == $regiestration){
                             $regiestration_class = 'active';
-                        }else if ()
+                        }else
 //Activation of the link pages
                         echo "<li class='$category_class'><a href='category.php?category={$cat_id}'>{$cat_title}</a></li>";
                     //now all the links can be shown active>>>>
