bbb895852e339485bea96febcb5a89a204b7b0f8
Samin Yeaser <saminyeaser1@gmail.com>
2019-11-29 12:42:29 +0600

    Now subscriber cant edit others profile

--- admin/includess/view_all_users.php
+++ admin/includess/view_all_users.php
@@ -64,7 +64,12 @@
 //        echo "<td>$comment_date</td>";
         echo "<td><a href='users.php?change_to_admin={$user_id}'>Admin</a></td>";
         echo "<td><a href='users.php?change_to_subscriber={$user_id}'>Subscriber</a></td>";
-        echo "<td><a href='users.php?source=edit_user&p_id={$user_id}'>Edit</a></td>";
+        if(isset($_SESSION['user_role'])){
+            if ($_SESSION['user_role'] == 'Admin'){
+                echo "<td><a href='users.php?source=edit_user&p_id={$user_id}'>Edit</a></td>";
+            }
+        }
+
         echo "<td><a onclick=\"javascript: return confirm('Are you sure you want to delete this post?');\" href='users.php?delete={$user_id}'>Delete</a></td>";
 //        echo "</tr>";
 
