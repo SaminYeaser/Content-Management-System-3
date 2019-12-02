26d6f811f0ff7a5a7ea34edf780446cb422c6325
Samin Yeaser <saminyeaser1@gmail.com>
2019-12-02 21:28:24 +0600

    welcome line change in post pages

--- admin/posts.php
+++ admin/posts.php
@@ -15,7 +15,7 @@
                 <div class="col-lg-12 col-xs-12">
 
                     <h1 class="page-header">
-                        Welcome to Admin
+                        Welcome to post dashboard
                         <small><?php echo $_SESSION['user_name']?></small>
                     </h1>
                     <?php
